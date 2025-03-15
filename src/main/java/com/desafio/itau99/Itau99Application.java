1473117cbc92b43bc1b16ffb6ea0d9b06ac01e89
jose.fernando <fernandoschnneider@gmail.com>
2025-03-15 13:36:37 -0400

    init spring

---
+++ src/main/java/com/desafio/itau99/Itau99Application.java
@@ -0,0 +1,13 @@
+package com.desafio.itau99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itau99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Itau99Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/itau99/Itau99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.itau99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itau99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
